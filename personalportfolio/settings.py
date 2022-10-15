3e52d3b0cc6658def4270fd2c25bec9ef296a254
sagar sangwan <iamsagarsangwan@gmail.com>
2022-10-16 03:49:05 +0530

    changed secret key

--- personalportfolio/settings.py
+++ personalportfolio/settings.py
@@ -33,7 +33,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9!8xe(5osv+ig40zqq3m0*g1c&12au=@q*+*^8iulkv$$z)5q%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
