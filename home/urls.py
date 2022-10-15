4cc0a9448f3db60a9d81a6587e166aec917cc3c2
sagar sangwan <iamsagarsangwan@gmail.com>
2022-10-16 03:47:01 +0530

    initial commit for my portfolio

---
+++ home/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import *
+from django.conf import settings
+from django.conf.urls.static import static
+
+from django.urls import include
+from django.shortcuts import render
+urlpatterns = [
+    path('', home, name='home'),
+]
