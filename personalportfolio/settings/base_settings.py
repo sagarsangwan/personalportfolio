58fdf2fe003d24197898fab113e94221c7816f1a
sagar sangwan <iamsagarsangwan@gmail.com>
2022-10-16 23:27:56 +0530

    runs collectstatic

--- personalportfolio/settings/base_settings.py
+++ personalportfolio/settings/base_settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 WHITENOISE_USE_FINDERS = True
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'personalportfolio/staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, '/static'), ]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
