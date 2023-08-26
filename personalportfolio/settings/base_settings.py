955703a7471c4b104951f5def83e637edc1bc988
sagar sangwan <iamsagarsangwan@gmail.com>
2023-08-26 16:32:09 +0530

    chaanges in local and prod settinggs

--- personalportfolio/settings/base_settings.py
+++ personalportfolio/settings/base_settings.py
@@ -135,12 +135,4 @@ USE_TZ = True
 
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-
-STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "/static"),
-]
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-MEDIA_URL = "/media/"
+load_dotenv(".env")
--- personalportfolio/settings/prod_settings.py
+++ personalportfolio/settings/prod_settings.py
@@ -11,9 +11,11 @@ load_dotenv("../.env")
 DEBUG = False
 ALLOWED_HOSTS = [".vercel.app", "sagarsangwan.vercel.app", ".now.sh"]
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles", "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", "static")
 
-load_dotenv(".env")
+STATIC_URL = "static/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
 
 DATABASES = {
     "default": {
