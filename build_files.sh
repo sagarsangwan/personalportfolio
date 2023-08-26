c252454ce8986dab6225cc88d1cb64d1e40e9911
sagar sangwan <iamsagarsangwan@gmail.com>
2023-08-26 17:21:27 +0530

    removed clear

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,9 @@
-# build_files.sh
+# 
+echo "build_files.sh"
 pip install -r requirements.txt
 python3.9 manage.py makemigrations 
 python3.9 manage.py migrate
 echo "collecting static files"
 
-python3.9 manage.py collectstatic --noinput --clear
+python3.9 manage.py collectstatic --noinput 
 echo "Done collecting static files"
