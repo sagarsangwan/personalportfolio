44e510a8c54893d6fa4e098ebfe1d60211037a65
sagar sangwan <iamsagarsangwan@gmail.com>
2023-08-26 06:09:01 +0530

    ????

--- personalportfolio/settings/prod_settings.py
+++ personalportfolio/settings/prod_settings.py
@@ -11,7 +11,7 @@ load_dotenv("../.env")
 DEBUG = False
 ALLOWED_HOSTS = [".vercel.app", "sagarsangwan.vercel.app", ".now.sh"]
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "/static"),)
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", "/static")
 
 load_dotenv(".env")
 
