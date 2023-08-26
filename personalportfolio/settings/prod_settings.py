4d374d2687969f8c760788fb06efe43f19676909
sagar sangwan <iamsagarsangwan@gmail.com>
2023-08-26 17:44:32 +0530

    ?????

--- personalportfolio/settings/prod_settings.py
+++ personalportfolio/settings/prod_settings.py
@@ -13,9 +13,9 @@ ALLOWED_HOSTS = [".vercel.app", "sagarsangwan.vercel.app", ".now.sh"]
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles", "static")
 #
-STATIC_URL = "static/"
-# MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-# MEDIA_URL = "/media/"
+STATIC_URL = "/static/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
 
 DATABASES = {
     "default": {
--- personalportfolio/urls.py
+++ personalportfolio/urls.py
@@ -21,11 +21,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
 from django.conf.urls import handler400, handler403, handler404, handler500
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("", include("home.urls")),
     path("admin/", admin.site.urls),
 ]
-
+# add at the last
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
