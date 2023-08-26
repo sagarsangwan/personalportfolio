c1755367b08d91c4267acbbae53b62afa0f6a40d
sagar sangwan <iamsagarsangwan@gmail.com>
2023-08-26 17:23:00 +0530

    changes in build files

--- build_files.sh
+++ build_files.sh
@@ -1,9 +1,6 @@
-# 
-echo "build_files.sh"
+
 pip install -r requirements.txt
 python3.9 manage.py makemigrations 
 python3.9 manage.py migrate
-echo "collecting static files"
 
 python3.9 manage.py collectstatic --noinput 
-echo "Done collecting static files"
