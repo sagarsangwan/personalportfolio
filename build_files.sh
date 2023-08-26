4596138d70860674be1ef65ab18b67eb1870deab
sagar sangwan <iamsagarsangwan@gmail.com>
2023-08-26 17:00:35 +0530

    added echo command

--- build_files.sh
+++ build_files.sh
@@ -2,4 +2,7 @@
 pip install -r requirements.txt
 python3.9 manage.py makemigrations 
 python3.9 manage.py migrate
+echo "collecting static files"
+
 python3.9 manage.py collectstatic --noinput --clear
+echo "Done collecting static files"
